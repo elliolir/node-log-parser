491417b020e0c6653cc4bdf400e2507c8a34196c
elliolir <dz.bartalevich@gmail.com>
2023-03-01 00:27:40 +0300

    add argument parsing and output file write

--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,82 @@
-const logFile = 'app.log';
-
 import { open } from 'node:fs/promises';
+import fs from 'node:fs';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 
-const parseLine = (line: string) => {
+enum Levels {
+  info = 'info',
+  debug = 'debug',
+  warn = 'warn',
+  error = 'error'
+}
+
+interface Log extends Record<string, any> {
+  timestamp: Date;
+  logLevel: Levels;
+  transactionId: string;
+  details: string;
+  err?: string;
+}
+
+const argv = yargs(hideBin(process.argv))
+    .options({
+      input: {
+        type: 'string',
+        default: 'app.log',
+        demandOption: true,
+        description: 'Path to the log file'
+      },
+      output: {
+        type: 'string',
+        default: 'errors.json',
+        demandOption: true,
+        description: 'Path to the generated JSON'
+      },
+      logLevels: {
+        type: 'array',
+        alias: 'l',
+        choices: Object.values(Levels),
+        default: Levels.error,
+        description: 'Filter by log level'
+      }
+    })
+    .parseSync();
+
+const { input, output, logLevels } = argv;
+
+const parseLine = (line: string): Log => {
   const [timestamp, logLevel, logMessage] = line.split(' - ');
   const message = JSON.parse(logMessage);
+
   return { timestamp, logLevel, ...message };
 };
 
-const parseLogs = async () => {
-  const file = await open(logFile);
-
-  for await (const line of file.readLines()) {
-    console.log(parseLine(line));
-  }
+const generateOutputLine = (log: Log, firstLine: boolean) => {
+  return `${!firstLine ? ',' : ''}\n  ` + JSON.stringify(log);
 };
 
-parseLogs();
+const parseLogs = async () => {
+  const inputFile = await open(input);
+  const outputFile = fs.createWriteStream(output);
+  outputFile.write('[');
+
+  let firstLine = true;
+
+  for await (const line of inputFile.readLines()) {
+    const log = parseLine(line);
+
+    if (logLevels.includes(log.logLevel)) {
+      outputFile.write(generateOutputLine(log, firstLine));
+
+      if (firstLine) firstLine = false;
+    }
+  }
+  outputFile.write('\n]');
+};
+
+parseLogs()
+    .then(() => console.log('Parsed!'))
+    .catch((err) => {
+      console.error(err);
+      console.log('Failed');
+    });
